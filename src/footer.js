0b55fed3510f16109b61c7d34ebedd2b5af0a4f2
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-01-11 17:28:25 -0800

    compleated core of assignment will clean later

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer>
+        <h3>Matt Hudson 2021</h3>
+      </footer>
+    )
+  }
+}
+export default Footer;
