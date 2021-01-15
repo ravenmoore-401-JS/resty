2bcb52c6e277b2fcd4cc2e072116e25e8cfc0d2f
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-01-14 23:25:15 -0800

    got some of the wedsday lab done but needs lots more work

--- src/api-results/api-results.js
+++ src/api-results/api-results.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactJson from 'react-json-view';
 
+import './api-results.scss';
+
 class Results extends React.Component{
   render(){
     return(
       <div id='apiResults'>
         <h3>API RESULTS</h3>
-        <span data-testId='reslutsWindow'><ReactJson src={this.props.apiResults}></ReactJson></span>
+        <span data-testId='reslutsWindow'>
+          <h5>Headers</h5>
+          <ReactJson src={this.props.apiHeaders}></ReactJson>
+          <h5>Results</h5>
+          <ReactJson src={this.props.apiResults}></ReactJson>
+        </span>
       </div>
     )
   }
